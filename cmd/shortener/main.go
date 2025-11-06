09f84b7450b9b5d5e77ffc061171d07026de17c6
Vishnyakov.E.A <vishnyakov.e.a@sbertech.ru>
2025-11-06 09:48:59 +0300

    iter1. Initial

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"github.com/Hizeqwane/Go-shortener-tpl/internal/handler"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	shortenerHandler := handler.NewHandler()
+
+	mux.Handle("/", shortenerHandler)
+
+	http.ListenAndServe(":8080", mux)
+}
---
+++ internal/handler/handler.go
@@ -0,0 +1,56 @@
+package handler
+
+import (
+	"github.com/Hizeqwane/Go-shortener-tpl/internal/service"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type RequestHandler struct {
+	shortenerService service.IShortenerService
+}
+
+func NewHandler() *RequestHandler {
+	return &RequestHandler{
+		shortenerService: service.NewShortenerService(),
+	}
+}
+
+func (p *RequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		if r.Header.Get("Content-Type") != "text/plain" {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+		bodyBytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "failed to read body", http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+
+		longUri := string(bodyBytes)
+
+		shortUri := p.shortenerService.GetShortUri(longUri)
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(shortUri))
+	}
+
+	if r.Method == http.MethodGet {
+		shortUri := strings.Trim(r.URL.Path, "/")
+
+		if i, ok := p.shortenerService.TryGetLongUri(shortUri); ok {
+			w.Header().Set("Location", i)
+			w.WriteHeader(http.StatusTemporaryRedirect)
+
+			return
+		}
+
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+var _ http.Handler = (*RequestHandler)(nil)
